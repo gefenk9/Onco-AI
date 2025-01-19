219939272e8ed05a08abfa6d5ef5e4bed16f18ef
gefenk9 <gefenk9@gmail.com>
2025-01-19 09:42:46 +0200

    changed the system prompt to explain the blood tests

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ for filename, content in guidelines_preview.items():
     file_selection_prompt += f"\n{'='*80}\n" f"GUIDELINE FILE: {filename}\n" f"{'='*80}\n" f"{content}\n" f"{'='*80}\n"
 
 file_selection_prompt += (
-    "\nBased on the following patient description, respond ONLY with the exact filename "
+    "\nBased on the following patient description, respond ONLY with exact filename list "
     "(including .txt extension) that best matches the patient's condition. "
     "Provide no other text in your response."
 )
@@ -66,10 +66,10 @@ SYSTEM_PROMPT_BASE = (
     "כרופא אונקולוג, אני זקוק לסיוע בגיבוש תוכנית טיפול מקיפה עבור מטופלים שאובחנו לאחרונה עם סרטן. "
     "אתה הולך לקבל מידע על המטופל. אנא ספק מתווה מפורט של אפשרויות טיפול פוטנציאליות, "
     "כולל משטרי כימותרפיה, גישות כירורגיות, שיקולי טיפול בקרינה, וטיפולים ממוקדים על בסיס "
-    "הנחיות אונקולוגיות עדכניות.בנוסף הצע בדיקות דם מתאימות לאבחנה, בנוסף, הצע אסטרטגיות לניהול תופעות לוואי נפוצות ותאר נקודות מפתח "
+    "הנחיות אונקולוגיות עדכניות.בנוסף הצע בדיקות דם מתאימות לאבחנה(תפרט בבקשה את הבדיקות באופן ספציפי), בנוסף, הצע אסטרטגיות לניהול תופעות לוואי נפוצות ותאר נקודות מפתח "
     "לחינוך המטופלת בנוגע לפרוגנוזה ושינויים באורח החיים. ענה בעברית בלבד. "
     "לכל המלצה הסבר את הסיבה להמלצה"
-    "להלן NCCN Guidelines לפיהן עליך לענות:"
+    "להלן NCCN Guidelines וESMO Guidelinesלפיהן עליך לענות:"
 )
 
 # Concatenate the base prompt with the guidelines content
@@ -77,6 +77,8 @@ SYSTEM_PROMPT = f"{SYSTEM_PROMPT_BASE}\n\n{guidelines_content}\n\n"
 
 with open('./user_prompt.txt', 'r', encoding='utf-8') as file:
     user_prompt = file.read()
+
+print ("\n\nLLM+RAG\n\n")
 
 message = client.messages.create(
     model="claude-3-5-sonnet-20241022",
@@ -88,14 +90,14 @@ message = client.messages.create(
 
 print(message.content[0].text)
 
-# print ("\n\nLLM+SYSTEM+RAG\n\n")
-#
-# message_rag = client.messages.create(
-#     model="claude-3-5-sonnet-20241022",
-#     max_tokens=1000,
-#     temperature=0,
-#     system="RAG_PROMPT",
-#     messages=[{"role": "user", "content": [{"type": "text", "text": user_prompt}]}],
-# )
-#
-# print(message_rag.content[0].text)
+print ("\n\nLLM\n\n")
+
+message_rag = client.messages.create(
+    model="claude-3-5-sonnet-20241022",
+    max_tokens=1000,
+    temperature=0,
+    system="SYSTEM_PROMPT_BASE",
+    messages=[{"role": "user", "content": [{"type": "text", "text": user_prompt}]}],
+)
+
+print(message_rag.content[0].text)
